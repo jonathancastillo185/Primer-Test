543049f8635785890ab32538645cb9388b4068a5
jonathancastillo185 <jonathancastillo185@email.com>
2023-12-02 04:19:29 -0300

    prints

--- secuencia.py
+++ secuencia.py
@@ -7,19 +7,25 @@ from datetime import datetime
 import warnings
 warnings.filterwarnings('ignore')
 
+print('-'*20)
 
 print(datetime.now())
+
+print('-'*20)
 extract_businesses()
 get_reviewsYelp_API()
-print('Paso 1 extraccion, completo')
-
+print('-'*5)
+print('-Paso 1 extraccion, completo-')
+print('-'*5)
 
 transform_business()
 trasnform_reviews_yelp()
-print('Paso 2 transformacion, completo')
-
+print('-'*5)
+print('-Paso 2 transformacion, completo-')
+print('-'*5)
 
 yelp_ER()
 yelp_review_ER()
-print('Paso 3 carga, completo')
-
+print('-'*5)
+print('-Paso 3 carga, completo-')
+print('-'*5)
