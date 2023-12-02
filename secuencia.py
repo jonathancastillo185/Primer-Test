53666c0ed5350aa01bc53f1cf5c6908e3ea42148
jonathancastillo185 <jonathancastillo185@email.com>
2023-12-02 03:25:21 -0300

    ultimo

--- secuencia.py
+++ secuencia.py
@@ -2,3 +2,19 @@ from mysql_connection import *
 from api_yelp import *
 from transform_API import *
 from etl_pipeline import *
+import time
+
+
+extract_businesses()
+get_reviewsYelp_API()
+print('Paso 1 extraccion, completo')
+time.sleep(60)
+
+transform_business()
+trasnform_reviews_yelp()
+print('Paso 2 transformacion, completo')
+time.sleep(60)
+
+yelp_ER()
+yelp_review_ER()
+print('Paso 3 carga, completo')
